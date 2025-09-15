0eea7ecb0cf68eceed7f91e6857c7cdb9f4b854b
majosetoro <toromajose@gmail.com>
2025-09-14 23:21:40 -0300

    feat: cambio nombre de carpeta a bottomNavbar en import index.js

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,7 +1,7 @@
 // Exportar todos los componentes principales
 export { default as ScreenHeader } from './ScreenHeader';
 export { default as FloatingMessage } from './FloatingMessage';
-export { default as BottomNavbar } from './BottomNavbar';
+export { default as BottomNavbar } from './bottomNavbar';
 
 // Re-exportar componentes de inputs y buttons
 export * from './inputs';
