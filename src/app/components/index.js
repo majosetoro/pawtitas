36cb5895ee240ef18d248a17d95318e67d269ac0
majosetoro <toromajose@gmail.com>
2025-09-16 22:09:22 -0300

    feat: implementar export en index.js de PerfilHeader.js

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -2,6 +2,7 @@
 export { default as ScreenHeader } from './ScreenHeader';
 export { default as FloatingMessage } from './FloatingMessage';
 export { default as BottomNavbar } from './BottomNavbar';
+export { default as PerfilHeader } from './PerfilHeader';
 
 // Re-exportar componentes de inputs y buttons
 export * from './inputs';
