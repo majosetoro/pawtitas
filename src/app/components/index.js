2e79cad70a9eaf9eed523e747286379c5be38cf6
majosetoro <toromajose@gmail.com>
2025-09-20 00:11:39 -0300

    feat: exportar componente reutilizable ConfirmacionDialogo.js en index.js

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,6 +1,7 @@
 // Exportar todos los componentes principales
 export { default as ScreenHeader } from './ScreenHeader';
 export { default as FloatingMessage } from './FloatingMessage';
+export { default as ConfirmacionDialogo } from './ConfirmacionDialogo';
 export { default as BottomNavbar } from './BottomNavbar';
 export { default as PerfilHeader } from './PerfilHeader';
 export { default as FloatingAddBtn } from './buttons/FloatingAddBtn';
