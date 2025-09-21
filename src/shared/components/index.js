aca91bc18b2a031b00c28bb5c20cb4415722a5dd
Andrea Paez <andreapaez-personal@MacBook-Pro-de-Andrea.local>
2025-09-21 20:49:10 -0300

    chore: eliminar Firebase y limpiar referencias del proyecto

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -1,7 +1,4 @@
 // Índice de componentes compartidos
 // Este archivo exporta todos los componentes compartidos para facilitar las importaciones
 
-export { default as FirebaseStatus } from './FirebaseStatus';
 
-// Este patrón de barrel exports facilita las importaciones:
-// import { FirebaseStatus } from '@shared/components';
