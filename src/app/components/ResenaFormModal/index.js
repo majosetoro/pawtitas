4c6c4494d4560e09fd013953626a41c0e9151022
majosetoro <toromajose@gmail.com>
2025-10-19 14:36:42 -0300

    feat: implementar modal para agregar reseña

---
+++ src/app/components/ResenaFormModal/index.js
@@ -0,0 +1,6 @@
+export { default as ResenaFormModal } from './ResenaFormModal';
+export { default } from './ResenaFormModal';
+export { SeccionInfoUsuario } from './SeccionInfoUsuario';
+export { SeccionCalificacion } from './SeccionCalificacion';
+export { SeccionComentario } from './SeccionComentario';
+export { default as CalificacionEstrellas } from './CalificacionEstrellas';
